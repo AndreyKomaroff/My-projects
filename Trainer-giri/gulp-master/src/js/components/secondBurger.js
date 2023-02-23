32496a2327eaafe3b23a096eeb02c00fe2fcd436
AndreyKomaroff <106623714+AndreyKomaroff@users.noreply.github.com>
2023-02-23 18:37:54 +0300

    Delete Trainer-giri/gulp-master directory

--- Trainer-giri/gulp-master/src/js/components/secondBurger.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("burger").addEventListener("click", function() {
-    document.getElementById("header").classList.toggle("__open")
-  })
-})
--- Trainer-giri/gulp-master/src/js/main.js
+++
@@ -1,4 +0,0 @@
-import './_vendor';
-import './_vars';
-import './_functions';
-import './_components';
