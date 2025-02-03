27295511f6b20decb3f2fa80217104ac2996c3e6
Andrey <komarov.andrew1988@gmail.com>
2025-02-03 10:55:41 +0300

    03.02

--- Нина Шайх/js/main.js
+++ Нина Шайх/js/main.js
@@ -18,3 +18,15 @@ for (let accordeon of accordeons) {
     }
   });
 };
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
